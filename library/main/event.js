47b44adb4303e703e6e4b8e4a1db0c108f1ea997
snowmantw <snowmantw@gmail.com>
2013-05-07 00:13:32 +0800

    Event issues

--- library/main/event.js
+++ library/main/event.js
@@ -1,8 +1,12 @@
+// Application event manager.
+// Independent from native events.
+
 (function()
 {
 window.Event = 
 {
-     trie: new Trie({})
+    // {type: {id: cb}}
+    callbacks: {}
 
     // EventName (string) -> Object -> {name: EventName, data: Object}
     ,genEvent: function(name, data)
@@ -13,47 +17,53 @@ window.Event =
     // Generate simple ID.
     ,newid: function()
     {
-        return 0
+        return "0"
     }
 
-    // EventName -> Event -> Number ( event binding ID )
+    // EventName -> Event -> Callback -> Number ( event binding ID )
     ,bind: function(ename, cb)
     {
+        if( undefined === Event.callbacks[ename] )
+        {
+            Event.callbacks[ename] = {}
+        }
         var id = Event.newid()
-        Event.newid = function(){ return id+1 } // Update the function.
-        Event.trie.update(ename+String(id), cb)
+        Event.newid = function(){ return String(Number(id)+1) } // Update the function.
+        Event.callbacks[ename][id] = cb
         return id
     }
 
     // ID is optional.
     // If it's omited, will unbind all callbacks under the event.
+    // Will return unbound callbacks.
     ,unbind: function(ename, id)
     {
+        var cbs   =  [] 
+        var sid   = id
         if( undefined == id )
         {
-            var binds = Event.trie.query(ename)
+            var binds = Event.callbacks[ename]
             for( var id in binds )
             {
-                // Default value is "".
-                Event.trie.update(ename+String(id), "")
+                cbs.push(binds[id])
+                delete Event.callbacks[ename][id]
             }
         }       
         else
         {
-            // Default value is "".
-            Event.trie.update(ename+String(id), "")
+            cbs.push( Event.callbacks[ename][id] )
+            delete Event.callbacks[ename][id]
         }
+        return cbs
     }
 
     ,trigger: function(e)
     {
-        var binds = Event.trie.query(e['name'])
+        var binds = Event.callbacks[e['name']]
         for( var id in binds )
         {
             // Find all bound callbacks in the event.
-            if( "" != id )
-            { 
-            cb = binds[id][""]
+            cb = binds[id]
             if("function" === typeof cb)
             {
             try{
@@ -61,7 +71,7 @@ window.Event =
             } catch(err){
                 throw "ERROR while handle event: "+e['name']+" with callback#"+id+", exception: "+err
             }
-            }}
+            }
         }
     }
 }
